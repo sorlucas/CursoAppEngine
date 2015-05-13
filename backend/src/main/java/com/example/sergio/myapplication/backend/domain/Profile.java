1cb454589a8a3b3469c4816ae600cce98fbeeb0b
sergio <sergio.lucas.lopez@gmail.com>
2015-05-13 14:56:13 +0200

    Add Skeletom lesson 2

---
+++ backend/src/main/java/com/example/sergio/myapplication/backend/domain/Profile.java
@@ -0,0 +1,52 @@
+package com.example.sergio.myapplication.backend.domain;
+
+
+import com.example.sergio.myapplication.backend.form.ProfileForm;
+
+
+// TODO indicate that this class is an Entity
+public class Profile {
+    String displayName;
+    String mainEmail;
+    ProfileForm.TeeShirtSize teeShirtSize;
+
+    // TODO indicate that the userId is to be used in the Entity's key
+    String userId;
+
+    /**
+     * Public constructor for Profile.
+     * @param userId The user id, obtained from the email
+     * @param displayName Any string user wants us to display him/her on this system.
+     * @param mainEmail User's main e-mail address.
+     * @param teeShirtSize The User's tee shirt size
+     *
+     */
+    public Profile (String userId, String displayName, String mainEmail, ProfileForm.TeeShirtSize teeShirtSize) {
+        this.userId = userId;
+        this.displayName = displayName;
+        this.mainEmail = mainEmail;
+        this.teeShirtSize = teeShirtSize;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getMainEmail() {
+        return mainEmail;
+    }
+
+    public ProfileForm.TeeShirtSize getTeeShirtSize() {
+        return teeShirtSize;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * Just making the default constructor private.
+     */
+    private Profile() {}
+
+}
