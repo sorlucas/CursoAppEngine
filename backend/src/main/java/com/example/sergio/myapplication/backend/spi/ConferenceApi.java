edde9567cb2f393f13f53838e7b89354a665c79c
sergio <sergio.lucas.lopez@gmail.com>
2015-05-15 20:07:54 +0200

    Add create Conference

--- backend/src/main/java/com/example/sergio/myapplication/backend/spi/ConferenceApi.java
+++ backend/src/main/java/com/example/sergio/myapplication/backend/spi/ConferenceApi.java
@@ -2,7 +2,9 @@ package com.example.sergio.myapplication.backend.spi;
 
 
 import com.example.sergio.myapplication.backend.Constants;
+import com.example.sergio.myapplication.backend.domain.Conference;
 import com.example.sergio.myapplication.backend.domain.Profile;
+import com.example.sergio.myapplication.backend.form.ConferenceForm;
 import com.example.sergio.myapplication.backend.form.ProfileForm;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -10,9 +12,10 @@ import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
 import com.googlecode.objectify.Key;
-import com.googlecode.objectify.ObjectifyService;
 
-import static com.googlecode.objectify.ObjectifyService.ofy;
+import static com.example.sergio.myapplication.backend.services.OfyService.factory;
+import static com.example.sergio.myapplication.backend.services.OfyService.ofy;
+
 
 /**
  * Defines conference APIs.
@@ -21,17 +24,32 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
         Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, description = "API for the Conference Central Backend application.")
 public class ConferenceApi {
 
-    static {
-        // Typically you would register this inside an OfyServive wrapper. See: https://code.google.com/p/objectify-appengine/wiki/BestPractices
-        ObjectifyService.register(Profile.class);
-    }
-
     /*
      * Get the display name from the user's email. For example, if the email is
      * lemoncake@example.com, then the display name becomes "lemoncake."
      */
     private static String extractDefaultDisplayNameFromEmail(String email) {
         return email == null ? null : email.substring(0, email.indexOf("@"));
+    }
+
+    /**
+     * Gets the Profile entity for the current user
+     * or creates it if it doesn't exist
+     * @param user
+     * @return user's Profile
+     */
+    private static Profile getProfileFromUser(User user) {
+        // First fetch the user's Profile from the datastore.
+        Profile profile = ofy().load().key(
+                Key.create(Profile.class, user.getUserId())).now();
+        if (profile == null) {
+            // Create a new Profile if it doesn't exist.
+            // Use default displayName and teeShirtSize
+            String email = user.getEmail();
+            profile = new Profile(user.getUserId(),
+                    extractDefaultDisplayNameFromEmail(email), email, ProfileForm.TeeShirtSize.NOT_SPECIFIED);
+        }
+        return profile;
     }
 
     /**
@@ -49,55 +67,44 @@ public class ConferenceApi {
 
     // Declare this method as a method available externally through Endpoints
     @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
-    // The request that invokes this method should provide data that
-    // conforms to the fields defined in ProfileForm
-
-    public Profile saveProfile(final User user, ProfileForm profileForm) throws UnauthorizedException {
-
-        // If the user is not logged in, throw an UnauthorizedException
-        if (user == null){
+    public Profile saveProfile(final User user, ProfileForm profileForm)
+            throws UnauthorizedException {
+        if (user == null) {
             throw new UnauthorizedException("Authorization required");
         }
 
         // Get the userId and mainEmail
-        String userId = user.getUserId();
         String mainEmail = user.getEmail();
+        String userId = user.getUserId();
 
-        // Get the displayName and teeShirtSize sent by the request
+        // Get the displayName and teeShirtSize sent by the request.
         String displayName = profileForm.getDisplayName();
         ProfileForm.TeeShirtSize teeShirtSize = profileForm.getTeeShirtSize();
 
-        //Get the Profile from the datastore if it exists otherwise create a new one
-        Profile profile = ofy().load().key(Key.create(Profile.class,userId))
-                .now();
+        // Get the Profile from the datastore if it exists otherwise create a new one
+        Profile profile = ofy().load().key(Key.create(Profile.class, userId)).now();
 
-        if (profile == null){
-            // If the displayName is null, set it to default value based on the user's email
-            // by calling extractDefaultDisplayNameFromEmail(...)
-            if (displayName == null){
-                displayName = extractDefaultDisplayNameFromEmail(user.getEmail());
+        if (profile == null) {
+            // Populate the displayName and teeShirtSize with default values if not sent in the request
+            if (displayName == null) {
+                displayName = extractDefaultDisplayNameFromEmail(user
+                        .getEmail());
             }
-
-            if (teeShirtSize == null){
+            if (teeShirtSize == null) {
                 teeShirtSize = ProfileForm.TeeShirtSize.NOT_SPECIFIED;
             }
-
             // Now create a new Profile entity
             profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
         } else {
-            //The Profile entity already exists. Update the Profile entity
+            // The Profile entity already exists. Update the Profile entity
             profile.update(displayName, teeShirtSize);
         }
 
-        //Save the entity in the datastore
+        // Save the entity in the datastore
         ofy().save().entity(profile).now();
 
         // Return the profile
         return profile;
-
-
-
-
     }
 
     /**
@@ -115,11 +122,53 @@ public class ConferenceApi {
         if (user == null) {
             throw new UnauthorizedException("Authorization required");
         }
-
         // load the Profile Entity
         String userId = user.getUserId();
-        Key key = Key.create(Profile.class,userId);
+        Key key = Key.create(Profile.class, userId);
+
         Profile profile = (Profile) ofy().load().key(key).now();
         return profile;
+    }
+
+    /**
+     * Creates a new Conference object and stores it to the datastore.
+     *
+     * @param user A user who invokes this method, null when the user is not signed in.
+     * @param conferenceForm A ConferenceForm object representing user's inputs.
+     * @return A newly created Conference Object.
+     * @throws UnauthorizedException when the user is not signed in.
+     */
+    @ApiMethod(name = "createConference", path = "conference", httpMethod = HttpMethod.POST)
+    public Conference createConference(final User user, final ConferenceForm conferenceForm)
+            throws UnauthorizedException {
+        if (user == null) {
+            throw new UnauthorizedException("Authorization required");
+        }
+
+        // Get the userId of the logged in User
+        String userId = user.getUserId();
+
+        // Get the key for the User's Profile
+        Key<Profile> profileKey = factory().allocateId(Profile.class);
+
+        // Allocate a key for the conference -- let App Engine allocate the ID
+        // Don't forget to include the parent Profile in the allocated ID
+        final Key<Conference> conferenceKey = factory().allocateId(profileKey,Conference.class);
+
+        // Get the Conference Id from the Key
+        final long conferenceId = conferenceKey.getId();
+
+        // Get the existing Profile entity for the current user if there is one
+        // Otherwise create a new Profile entity with default values
+        Profile profile = getProfileFromUser(user);
+
+        // Create a new Conference Entity, specifying the user's Profile entity
+        // as the parent of the conference
+        Conference conference = new Conference(conferenceId,profile.getUserId(),conferenceForm);
+
+        // Save Conference and Profile Entities
+        ofy().save().entities(profile,conference);
+
+        return conference;
     }
 }
