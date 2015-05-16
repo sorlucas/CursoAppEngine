4093c2f3f7c2c619f67820e9b1f63271663d75cd
sergio <sergio.lucas.lopez@gmail.com>
2015-05-16 18:07:35 +0200

    Fix ConferenceApi to use Web UI and add AppEngineUser

--- backend/src/main/java/com/example/sergio/myapplication/backend/spi/ConferenceApi.java
+++ backend/src/main/java/com/example/sergio/myapplication/backend/spi/ConferenceApi.java
@@ -2,6 +2,7 @@ package com.example.sergio.myapplication.backend.spi;
 
 
 import com.example.sergio.myapplication.backend.Constants;
+import com.example.sergio.myapplication.backend.domain.AppEngineUser;
 import com.example.sergio.myapplication.backend.domain.Conference;
 import com.example.sergio.myapplication.backend.domain.Profile;
 import com.example.sergio.myapplication.backend.form.ConferenceForm;
@@ -10,12 +11,19 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.appengine.api.users.User;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.Work;
 import com.googlecode.objectify.cmd.Query;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import static com.example.sergio.myapplication.backend.form.ProfileForm.TeeShirtSize;
 import static com.example.sergio.myapplication.backend.services.OfyService.factory;
 import static com.example.sergio.myapplication.backend.services.OfyService.ofy;
 
@@ -26,6 +34,8 @@ import static com.example.sergio.myapplication.backend.services.OfyService.ofy;
 @Api(name = "conference", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = {
         Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, description = "API for the Conference Central Backend application.")
 public class ConferenceApi {
+
+    private static final Logger LOG = Logger.getLogger(ConferenceApi.class.getName());
 
     /*
      * Get the display name from the user's email. For example, if the email is
@@ -41,95 +51,87 @@ public class ConferenceApi {
      * @param user
      * @return user's Profile
      */
-    private static Profile getProfileFromUser(User user) {
-        // First fetch the user's Profile from the datastore.
+    private static Profile getProfileFromUser(User user, String userId) {
+        // First fetch it from the datastore.
         Profile profile = ofy().load().key(
-                Key.create(Profile.class, user.getUserId())).now();
+                Key.create(Profile.class, userId)).now();
         if (profile == null) {
-            // Create a new Profile if it doesn't exist.
-            // Use default displayName and teeShirtSize
+            // Create a new Profile if not exist.
             String email = user.getEmail();
-            profile = new Profile(user.getUserId(),
-                    extractDefaultDisplayNameFromEmail(email), email, ProfileForm.TeeShirtSize.NOT_SPECIFIED);
+            profile = new Profile(userId,
+                    extractDefaultDisplayNameFromEmail(email), email, TeeShirtSize.NOT_SPECIFIED);
         }
         return profile;
     }
 
     /**
-     * Creates or updates a Profile object associated with the given user
-     * object.
+     * This is an ugly workaround for null userId for Android clients.
      *
-     * @param user
-     *            A User object injected by the cloud endpoints.
-     * @param profileForm
-     *            A ProfileForm object sent from the client form.
-     * @return Profile object just created.
-     * @throws UnauthorizedException
-     *             when the User object is null.
+     * @param user A User object injected by the cloud endpoints.
+     * @return the App Engine userId for the user.
      */
-
-    // Declare this method as a method available externally through Endpoints
-    @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
-    public Profile saveProfile(final User user, ProfileForm profileForm)
-            throws UnauthorizedException {
-        if (user == null) {
-            throw new UnauthorizedException("Authorization required");
-        }
-
-        // Get the userId and mainEmail
-        String mainEmail = user.getEmail();
+    private static String getUserId(User user) {
         String userId = user.getUserId();
-
-        // Get the displayName and teeShirtSize sent by the request.
-        String displayName = profileForm.getDisplayName();
-        ProfileForm.TeeShirtSize teeShirtSize = profileForm.getTeeShirtSize();
-
-        // Get the Profile from the datastore if it exists otherwise create a new one
-        Profile profile = ofy().load().key(Key.create(Profile.class, userId)).now();
-
-        if (profile == null) {
-            // Populate the displayName and teeShirtSize with default values if not sent in the request
-            if (displayName == null) {
-                displayName = extractDefaultDisplayNameFromEmail(user
-                        .getEmail());
-            }
-            if (teeShirtSize == null) {
-                teeShirtSize = ProfileForm.TeeShirtSize.NOT_SPECIFIED;
-            }
-            // Now create a new Profile entity
-            profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
-        } else {
-            // The Profile entity already exists. Update the Profile entity
-            profile.update(displayName, teeShirtSize);
+        if (userId == null) {
+            LOG.info("userId is null, so trying to obtain it from the datastore.");
+            AppEngineUser appEngineUser = new AppEngineUser(user);
+            ofy().save().entity(appEngineUser).now();
+            // Begin new session for not using session cache.
+            Objectify objectify = ofy().factory().begin();
+            AppEngineUser savedUser = objectify.load().key(appEngineUser.getKey()).now();
+            userId = savedUser.getUser().getUserId();
+            LOG.info("Obtained the userId: " + userId);
         }
-
-        // Save the entity in the datastore
-        ofy().save().entity(profile).now();
-
-        // Return the profile
-        return profile;
+        return userId;
     }
 
     /**
-     * Returns a Profile object associated with the given user object. The cloud
-     * endpoints system automatically inject the User object.
+     * Returns a Profile object associated with the given user object. The cloud endpoints system
+     * automatically inject the User object.
      *
-     * @param user
-     *            A User object injected by the cloud endpoints.
+     * @param user A User object injected by the cloud endpoints.
      * @return Profile object.
-     * @throws UnauthorizedException
-     *             when the User object is null.
+     * @throws UnauthorizedException when the User object is null.
      */
     @ApiMethod(name = "getProfile", path = "profile", httpMethod = HttpMethod.GET)
     public Profile getProfile(final User user) throws UnauthorizedException {
         if (user == null) {
             throw new UnauthorizedException("Authorization required");
         }
-        // load the Profile Entity
-        String userId = user.getUserId();
-        Key key = Key.create(Profile.class, userId);
+        return ofy().load().key(Key.create(Profile.class, getUserId(user))).now();
+    }
 
-        Profile profile = (Profile) ofy().load().key(key).now();
+    /**
+     * Creates or updates a Profile object associated with the given user object.
+     *
+     * @param user A User object injected by the cloud endpoints.
+     * @param profileForm A ProfileForm object sent from the client form.
+     * @return Profile object just created.
+     * @throws UnauthorizedException when the User object is null.
+     */
+    @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
+    public Profile saveProfile(final User user, final ProfileForm profileForm)
+            throws UnauthorizedException {
+        if (user == null) {
+            throw new UnauthorizedException("Authorization required");
+        }
+        String displayName = profileForm.getDisplayName();
+        TeeShirtSize teeShirtSize = profileForm.getTeeShirtSize();
+
+        Profile profile = ofy().load().key(Key.create(Profile.class, getUserId(user))).now();
+        if (profile == null) {
+            // Populate displayName and teeShirtSize with the default values if null.
+            if (displayName == null) {
+                displayName = extractDefaultDisplayNameFromEmail(user.getEmail());
+            }
+            if (teeShirtSize == null) {
+                teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
+            }
+            profile = new Profile(getUserId(user), displayName, user.getEmail(), teeShirtSize);
+        } else {
+            profile.update(displayName, teeShirtSize);
+        }
+        ofy().save().entity(profile).now();
         return profile;
     }
 
@@ -147,33 +149,31 @@ public class ConferenceApi {
         if (user == null) {
             throw new UnauthorizedException("Authorization required");
         }
-
-        // Get the userId of the logged in User
-        String userId = user.getUserId();
-
-        // Get the key for the User's Profile
-        Key<Profile> profileKey = factory().allocateId(Profile.class);
-
-        // Allocate a key for the conference -- let App Engine allocate the ID
-        // Don't forget to include the parent Profile in the allocated ID
-        final Key<Conference> conferenceKey = factory().allocateId(profileKey,Conference.class);
-
-        // Get the Conference Id from the Key
+        // Allocate Id first, in order to make the transaction idempotent.
+        Key<Profile> profileKey = Key.create(Profile.class, getUserId(user));
+        final Key<Conference> conferenceKey = factory().allocateId(profileKey, Conference.class);
         final long conferenceId = conferenceKey.getId();
-
-        // Get the existing Profile entity for the current user if there is one
-        // Otherwise create a new Profile entity with default values
-        Profile profile = getProfileFromUser(user);
-
-        // Create a new Conference Entity, specifying the user's Profile entity
-        // as the parent of the conference
-        Conference conference = new Conference(conferenceId,profile.getUserId(),conferenceForm);
-
-        // Save Conference and Profile Entities
-        ofy().save().entities(profile,conference);
-
+        final Queue queue = QueueFactory.getDefaultQueue();
+        final String userId = getUserId(user);
+        // Start a transaction.
+        Conference conference = ofy().transact(new Work<Conference>() {
+            @Override
+            public Conference run() {
+                // Fetch user's Profile.
+                Profile profile = getProfileFromUser(user, userId);
+                Conference conference = new Conference(conferenceId, userId, conferenceForm);
+                // Save Conference and Profile.
+                ofy().save().entities(conference, profile).now();
+                queue.add(ofy().getTransaction(),
+                        TaskOptions.Builder.withUrl("/tasks/send_confirmation_email")
+                                .param("email", profile.getMainEmail())
+                                .param("conferenceInfo", conference.toString()));
+                return conference;
+            }
+        });
         return conference;
     }
+
 
     @ApiMethod(
             name = "queryConferences",
@@ -195,7 +195,7 @@ public class ConferenceApi {
             throw new UnauthorizedException("Authorization required");
         }
         // Get the key for the User's Profile
-        String userId = user.getUserId();
+        String userId = getUserId(user);
         Key userKey = Key.create(Profile.class, userId);
 
         return ofy().load().type(Conference.class)
